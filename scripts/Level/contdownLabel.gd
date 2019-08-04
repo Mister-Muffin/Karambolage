e038445408c8f4f8eb4bd8dc32d345b31d9e7440
MarkusMCPEGamer <markus.aa84@gmail.com>
2019-08-04 13:52:46 +0200

    Improvements
    # Improvements to:
    
    -  added Animation to: Death/Game end

--- scripts/Level/contdownLabel.gd
+++ scripts/Level/contdownLabel.gd
@@ -1,13 +1,18 @@
 extends Label
 
+var game_finished = false
+
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	if GLOBALS.countDown && not get_node("animPlayer").is_playing():
-		if not get_node("animPlayer").is_playing(): get_node("animPlayer").play("countdown")
-		self.visible = true
-	if not GLOBALS.countDown:
-		get_node("animPlayer").stop(true)
-		self.visible = false
-		#
+	if not game_finished:
+		if GLOBALS.countDown && not get_node("animPlayer").is_playing():
+			if not get_node("animPlayer").is_playing(): get_node("animPlayer").play("countdown")
+			self.visible = true
+		if not GLOBALS.countDown:
+			get_node("animPlayer").stop(true)
+			self.visible = false
+
+func _on_Player_end_game():
+	game_finished = true
