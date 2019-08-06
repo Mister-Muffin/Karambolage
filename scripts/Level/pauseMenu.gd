d53f13d10b0b99258c5790bf45995c055ed1cb0e
DaKie206 <kieferdavid@outlook.com>
2019-08-06 22:54:12 +0200

    not working

--- scripts/Level/pauseMenu.gd
+++ scripts/Level/pauseMenu.gd
@@ -4,13 +4,32 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
+	
+	var keyBindingAnim = $"../../keyBindingCanvas/animPlayer"
+	var keyBindingLabel = $"../../keyBindingCanvas/infoLabel"
+	
+	var labelVisible = keyBindingLabel.visible
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		if get_tree().paused:
 			get_tree().paused = false
 			$animPlayer.play_backwards("anim")
+			
+			if not labelVisible:
+				keyBindingAnim.play("blend")
+				keyBindingLabel.visible = false
+				
+			keyBindingLabel.text = "Esc: Pause"
+			
 		else:
 			$animPlayer.play("anim")
+			keyBindingLabel.text = "Esc: Continue"
+			if not labelVisible:
+				keyBindingLabel.visible = true
+				keyBindingAnim.play_backwards("blend")
 			get_tree().paused = true
+			
+			
 
 
 func _on_btnContinue_pressed():
