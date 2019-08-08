21fb80fd17baeb764f6796d64e57360d64baa97a
MarkusMCPEGamer <markus.aa84@gmail.com>
2019-08-08 17:33:43 +0200

    Update Start.gd
    Bug fixed

--- scripts/Start/Start.gd
+++ scripts/Start/Start.gd
@@ -1,22 +1,21 @@
 extends Control
 
-export var autostart = true
+var pressed = false
 
 func _on_Button_button_down():
 	GLOBALS.cave = false
 	get_tree().change_scene("res://scenes/Level.tscn")
 
-var pressed = false
-
 func _ready():
 	set_process(true)
 	$Splash/info/animPlayer.play("blend")
+	if GLOBALS.splashDone: switch()
 
 
 func _unhandled_key_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.scancode == KEY_ENTER:
-			switch()
+			$animPlayer.play("switch")
 			GLOBALS.splashDone = true
 	if event.pressed && event.scancode == 32:
 		GLOBALS.cave = false
@@ -46,10 +45,9 @@ func _on_Timer_timeout():
 	get_tree().change_scene("res://scenes/ReleaseToQuit.tscn")
 	
 func switch():
-	$animPlayer.play("switch")
-#	$Main.visible = true
-#	$Main/title.visible = true
-#	$Main/ModeContainer/CanvasLayer/btnPlay.visible = true
-#	$Main/ModeContainer/CanvasLayer2/btnPlayFast.visible = true
-#	$Main/ModeContainer/CanvasLayer3/btnCave.visible = true
-#	$Splash.visible = false
+	$Main.visible = true
+	$Main/title.visible = true
+	$Main/ModeContainer/CanvasLayer/btnPlay.visible = true
+	$Main/ModeContainer/CanvasLayer2/btnPlayFast.visible = true
+	$Main/ModeContainer/CanvasLayer3/btnCave.visible = true
+	$Splash.visible = false
