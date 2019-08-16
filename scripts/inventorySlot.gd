cf5cffa9e79ab08296ec7cbd7ced057bae3cc168
MarkusMCPEGamer <markus.aa84@gmail.com>
2019-08-16 11:41:11 +0200

    New Animations
    - Added animations to the inventory items

--- scripts/inventorySlot.gd
+++ scripts/inventorySlot.gd
@@ -12,7 +12,7 @@ func _ready():
 func _on_btn_button_down():
 	if hasItem && GLOBALS.health <= 80:
 		hasItem = false
-		$container/item.visible = false
+		$animPlayer.play_backwards("anim")
 		GLOBALS.health = GLOBALS.health + 20
 		$timer.start()
 		$progressBar.visible = true
@@ -22,12 +22,17 @@ func _process(delta):
 	if Input.is_action_just_pressed(String(slotNumber)):
 		if hasItem && GLOBALS.health <= 80:
 			hasItem = false
-			$container/item.visible = false
+			$animPlayer.play_backwards("anim")
 			GLOBALS.health = GLOBALS.health + 20
 			$timer.start()
 			$progressBar.visible = true
 
 func _on_timer_timeout():
 	hasItem = true
-	$container/item.visible = true
+	$animPlayer.play("anim")
 	$progressBar.visible = false
+
+
+func _on_animPlayer_animation_finished(anim_name):
+	#if not hasItem: $container/item.visible = false
+	pass
