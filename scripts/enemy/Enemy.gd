747e81bac5f2c9a6635d81ba5d9cf90893013581
MarkusMCPEGamer <markus.aa84@gmail.com>
2019-08-28 18:21:36 +0200

    Cleaned up code Important performance fixes

--- scripts/enemy/Enemy.gd
+++ scripts/enemy/Enemy.gd
@@ -1,20 +1,29 @@
 extends KinematicBody2D
 
 var detected = false
+var direction
+var move = false
+export (int) var speed = 100
 
 func _ready():
 	position = Vector2(rand_range(64, get_viewport_rect().size.x - 64), rand_range(64, get_viewport_rect().size.y - 64))
 	get_node("ExclamationMark").visible = false
 
 func _physics_process(delta):
-	if detected:
-		if not get_tree().paused:
+	if move:
+		move_and_collide(direction * speed * delta)
+
+func _process(delta):
+	if detected && not get_tree().paused:
+		if not $ExclamationMark.visible:
 			get_node("ExclamationMark").visible = true
-			get_node("ExclamationMark/animPlayer").play("axclamMark")
-			var direction = (GLOBALS.playerPos - global_position).normalized()
-			move_and_collide(direction * 100 * delta)
-			yield(get_tree(), "idle_frame")
-		else: yield(get_tree(), "idle_frame")
+		get_node("ExclamationMark/animPlayer").play("axclamMark")
+		direction = (GLOBALS.playerPos - global_position).normalized()
+		move = true
+		#yield(get_tree(), "idle_frame")
+	else:
+		move = false
+		#yield(get_tree(), "idle_frame")
 
 
 func _on_detectingArea_body_entered(body):
@@ -28,11 +37,7 @@ func _on_detectingArea_body_exited(body):
 func _on_collsisionArea_body_entered(body):
 	if not body.is_in_group("Player"): return
 	GLOBALS.enemysInCollision = GLOBALS.enemysInCollision + 1
-	#$Sprite.visible = false
-	#$particles.emitting = true
-	#GLOBALS.enemysInCollision = GLOBALS.enemysInCollision - 1
-	#yield(get_tree().create_timer(1.0, false), "timeout")
-	#queue_free()
+
 
 func _on_collsisionArea_body_exited(body):
 	if body.is_in_group("Player"): GLOBALS.enemysInCollision = GLOBALS.enemysInCollision - 1
