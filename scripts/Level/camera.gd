31ea53b75c3345f28ecfaa9c043a841a9df727fc
MarkusMCPEGamer <markus.aa84@gmail.com>
2019-08-28 20:16:17 +0200

    Player feedback improved
    - Camera shakes in case of damage

---
+++ scripts/Level/camera.gd
@@ -0,0 +1,35 @@
+extends Camera2D
+
+var tmpHealth = 100
+
+export var magnitude = 10.0
+export var lifeTime = 0.2
+var timeLeft = 1.0
+
+var isShaking = false
+
+func _ready():
+	tmpHealth = GLOBALS.health
+
+func _physics_process(delta):
+	if tmpHealth > GLOBALS.health:
+		shake(delta)
+		tmpHealth = GLOBALS.health
+	else:
+		tmpHealth = GLOBALS.health
+
+func shake(delta):
+	timeLeft = lifeTime
+
+	if isShaking: return
+	isShaking = true
+
+	while timeLeft > 0:
+		position = Vector2(rand_range(-magnitude, magnitude), rand_range(-magnitude, magnitude))
+
+		timeLeft -= delta
+		yield(get_tree(), "idle_frame")
+	
+	timeLeft = lifeTime
+	isShaking = false
+	position = Vector2(0, 0)
