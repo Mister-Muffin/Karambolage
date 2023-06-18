d04a1c389a9ae175da2ea6488a96986b2e2bae70
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2023-06-18 17:31:18 +0200

    Update inventorySlot.gd
    removed unused signal
    reorder code

--- scripts/inventorySlot.gd
+++ scripts/inventorySlot.gd
@@ -1,19 +1,15 @@
 class_name Slot
 extends Control
 
-var slotNumber: int = 0
-
 const health := 20
 
+var slotNumber: int = 0
 var itemCount: int = 0
 
 var type: Item.TYPES
 
-signal addEnergy
-
 func _ready():
-	$number.text = str(itemCount) # TODO: set to 0 in editor
-
+	$number.text = str(itemCount)
 
 func _process(delta):
 	if Input.is_action_just_pressed(str(slotNumber)) and type != null:
@@ -24,6 +20,7 @@ func _process(delta):
 			if type == Item.TYPES.ENERGY:
 				_updateEnergy()
 
+
 func _updateHealth():
 	GLOBALS.signal_change_health(health, 1)
 	GLOBALS.signal_change_health(health, 2)
@@ -33,10 +30,17 @@ func _updateEnergy():
 	GLOBALS.signal_change_energy(30, 2)
 
 
+func _remove_item():
+	if itemCount >= 1:
+		itemCount -= 1
+	$number.text = str(itemCount)
+	if itemCount <= 0:
+		$animPlayer.play_backwards("anim")
+
+
 func change_slot_number(number: int):
 	slotNumber = number
 	$slot.text = str(slotNumber)
-
 
 func add_item(t: Item.TYPES, texture: Resource):
 	type = t
@@ -47,11 +51,3 @@ func add_item(t: Item.TYPES, texture: Resource):
 	itemCount += 1
 	$number.text = str(itemCount)
 	$"container/item".texture = texture
-
-
-func _remove_item():
-	if itemCount >= 1:
-		itemCount -= 1
-	$number.text = str(itemCount)
-	if itemCount <= 0:
-		$animPlayer.play_backwards("anim")
